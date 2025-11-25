a9e6e6a23e59c6226d421a426b38e257b65cf4ea
Krzysztofr1 <krzysztof.rusel@infotech.edu.pl>
2025-11-25 10:08:41 +0100

    feat: Add Jazzmin for admin interface customization and register new models for behavior points and events

--- utils/admin.py
+++ utils/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Przedmiot, Temat, PracaDomowa, DataSource
 
-# Register your models here.
+
+@admin.register(Przedmiot)
+class PrzedmiotAdmin(admin.ModelAdmin):
+	list_display = ("id", "nazwa", "nazwa_skrocona", "numer")
+
+
+@admin.register(Temat)
+class TematAdmin(admin.ModelAdmin):
+	list_display = ("id", "tresc", "data", "przedmiot")
+
+
+@admin.register(PracaDomowa)
+class PracaDomowaAdmin(admin.ModelAdmin):
+	list_display = ("id", "klasa", "przedmiot", "nauczyciel", "data_wystawienia")
+
+
+@admin.register(DataSource)
+class DataSourceAdmin(admin.ModelAdmin):
+	list_display = ("id", "active_source", "last_import_file", "last_imported_at")
